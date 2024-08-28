6cf35ffd7e42e50f874d204aa4207e76f1b63abe
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-08-28 14:12:28 +0530

    Cyclic Sort Implemented

---
+++ 09. Sorting/Cyclic Sort/CyclicSort.java
@@ -0,0 +1,28 @@
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = { 7, 5, 2, 8, 6, 1, 4, 3, 9 };
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
